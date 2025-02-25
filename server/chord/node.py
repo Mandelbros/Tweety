d74fc542ad113faec2e0d01727c3d9043a25ae99
arielgg46 <lenin46ariel@gmail.com>
2025-02-25 06:55:54 -0500

    Added main server thread and handling of node operations

--- server/chord/node.py
+++ server/chord/node.py
@@ -1,10 +1,13 @@
 import logging
 import threading
+import socket
 
 from chord.utils import  getShaRepr
 from chord.node_ref import NodeRef
 from chord.bounded_list import BoundedList
 from chord.finger_table import FingerTable
+from chord.constants import *
+from config import SEPARATOR
 
 class Node:
     def __init__(self, ip: str, port: int = 8001, m: int = 160, c: int = 3):
@@ -21,6 +24,8 @@ class Node:
         self.pred_lock = threading.RLock()
 
         self.shutdown_event = threading.Event()
+
+        threading.Thread(target=self.start_server, daemon=True).start()
 
         self.finger = FingerTable(self, m)
 
@@ -41,3 +46,58 @@ class Node:
         logging.info(f'Remove key {key}')
 
         return 0
+    
+    def start_server(self):
+        """
+        Starts the main server thread to handle incoming client requests.
+        
+        The server listens for connections and processes different types of operations.
+        Operations include key retrieval, key storage, election, and more.
+        """
+        logging.info('Iniciando Hilo Principal del servidor y escuchando a conecciones...')
+        
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((self.ip, self.port))
+            s.listen(10)
+
+            while True:
+                try:
+                    conn, addr = s.accept()
+                    logging.info(f'Nueva conexión de {addr}')
+
+                    data = conn.recv(1024).decode().split(SEPARATOR)
+
+                    option = int(data[0])
+                    logging.info(f'Operación {option} recibida de {addr}')
+                    
+                    data_response = None
+
+                    # Handling various operations based on option value
+                    if option == FIND_ID_PREDECESSOR:
+                        id = int(data[1])
+                        pred = self.finger.find_predecessor(id)
+                        data_response = pred if pred else self.ref
+                    elif option == FIND_ID_SUCCESSOR:
+                        id = int(data[1])
+                        data_response = self.finger.find_successor(id)
+                    elif option == GET_PREDECESSOR:
+                        pred = self.predecessors.get(0)
+                        data_response = pred if pred else self.ref
+                    elif option == GET_SUCCESSOR:
+                        succ = self.successors.get(0)
+                        data_response = succ if succ else self.ref
+                    elif option == CLOSEST_PRECEDING_FINGER:
+                        id = int(data[1])
+                        data_response = self.finger.closest_preceding_finger(id)
+
+                    # Prepare the response to send to the client
+                    if data_response:
+                        response = f'{data_response.id}{SEPARATOR}{data_response.ip}'.encode()
+
+                    logging.info(f'Enviando respuesta: {response}')
+                    conn.sendall(response)
+                    conn.close()
+
+                except Exception as e:
+                    logging.error(f'Error en Hilo Principal del servidor: {e}')
--- server/chord/node_ref.py
+++ server/chord/node_ref.py
@@ -1,11 +1,101 @@
+import logging
+import socket
+
+from chord.constants import *
 from chord.utils import getShaRepr
+from config import PORT, SEPARATOR
 
 class NodeRef:
-    def __init__(self, ip: str, port: int = 10000):
-        self.id = getShaRepr(ip)
+    def __init__(self, ip: str, port: int = PORT):
+        """
+        Initializes a reference to a Chord node.
+
+        Args:
+            ip (str): IP address of the node.
+            port (int): Port number of the node (default is the value from config).
+        """
+        self.id = getShaRepr(ip)  # Unique identifier for the node based on its IP address
         self.ip = ip
         self.port = port
 
+    def process_operation(self, op: int, data: str = None) -> bytes:
+        """
+        Internal method to process a requested operation in the referenced node.
+
+        Args:
+            op (int): The operation code.
+            data (str): The data to send (default is None).
+
+        Returns:
+            bytes: The response received from the node.
+        """
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.settimeout(3)
+                s.connect((self.ip, int(self.port)))
+                s.sendall(f'{op}{SEPARATOR}{data}'.encode('utf-8'))
+                return s.recv(1024)
+        except Exception as e:
+            logging.error(f"Error enviando dato a {self.ip}: {e}, operación: {op}, dato: {data}")
+            return b''
+
+    def find_predecessor(self, id: int) -> 'NodeRef':
+        """
+        Finds the predecessor of a given id.
+
+        Args:
+            id (int): The id to find the predecessor for.
+
+        Returns:
+            NodeRef: A reference to the predecessor node.
+        """
+        response = self.process_operation(FIND_ID_PREDECESSOR, str(id)).decode().split(SEPARATOR)
+        return NodeRef(response[1], self.port)
+
+    def find_successor(self, id: int) -> 'NodeRef':
+        """
+        Finds the successor of a given id.
+
+        Args:
+            id (int): The id to find the successor for.
+
+        Returns:
+            NodeRef: A reference to the successor node.
+        """
+        response = self.process_operation(FIND_ID_SUCCESSOR, str(id)).decode().split(SEPARATOR)
+        return NodeRef(response[1], self.port)
+
+    @property
+    def pred(self) -> 'NodeRef':
+        """
+        Gets the predecessor of the current node.
+
+        Returns:
+            NodeRef: A reference to the predecessor node.
+        """
+        response = self.process_operation(GET_PREDECESSOR).decode().split(SEPARATOR)
+        return NodeRef(response[1], self.port)
+
+    @property
+    def succ(self) -> 'NodeRef':
+        """
+        Gets the successor of the current node.
+
+        Returns:
+            NodeRef: A reference to the successor node.
+        """
+        response = self.process_operation(GET_SUCCESSOR).decode().split(SEPARATOR)
+        return NodeRef(response[1], self.port)
+
     def closest_preceding_finger(self, id: int) -> 'NodeRef':
-        # return self
-        pass
+        """
+        Finds the closest preceding finger for a given id.
+
+        Args:
+            id (int): The id to find the closest preceding finger for.
+
+        Returns:
+            NodeRef: A reference to the closest preceding finger node.
+        """
+        response = self.process_operation(CLOSEST_PRECEDING_FINGER, str(id)).decode().split(SEPARATOR)
+        return NodeRef(response[1], self.port)
